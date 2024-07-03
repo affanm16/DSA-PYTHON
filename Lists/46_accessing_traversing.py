ccbbff3aa698387cfb9e72b6c63513ca80291d7d
affanm16 <affan24748@gmail.com>
2024-07-04 03:08:56 +0530

    accessing and traversing

---
+++ Lists/46_accessing_traversing.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5]
+mixed_list = [1, "hello", 3.14, True]
+nested_list = [[1, 2], [3, 4], [5, 6]]
+
+#accessing
+print(numbers[2])
+print(numbers[-4])
+
+#traversing
+print("traversing")
+for i in numbers:
+    print(i)
+
