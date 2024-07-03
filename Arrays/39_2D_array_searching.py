c9563b9f69c16f9d2924ee843aa340d7a33d7796
affanm16 <affan24748@gmail.com>
2024-07-04 01:20:53 +0530

    searching in 2d array(linear search)

---
+++ Arrays/39_2D_array_searching.py
@@ -0,0 +1,17 @@
+#linear search
+
+import numpy as np
+twoDArray=np.array( [ [21 ,35 ,40 ,37],[20, 38, 43, 29],[21, 30 ,39 ,38],[20 ,31, 50, 44] ] )
+print(twoDArray)
+
+def search_linear(array,value):
+    for i in range(len(array)):  #gives number of rows ->o(m)
+        for j in range(len(array[0])): #gives number of columns->O(n)
+            if array[i][j]==value:
+                return 'the element is located at index ' +str(i)+" "+str(j)
+    return 'the element is not found'
+
+search=search_linear(twoDArray,29)
+print(search)
+
+#time and space complexity is O(mxn),O(1) respectively
