87e8fde31d3422dc2fdc37a953a0248dd97f0e40
affanm16 <affan24748@gmail.com>
2024-07-03 23:49:13 +0530

    creation for 2d arrays

---
+++ Arrays/35_2D_array_creation.py
@@ -0,0 +1,13 @@
+# 2d array a[i][j] where i is row number and j is the column number
+# Day-1 21 35 40 37
+# Day-2 20 38 43 29
+# Day-3 21 30 39 38
+# Day-4 20 31 50 44
+
+#method-1 using numpy
+
+import numpy as np
+twoDArray=np.array( [ [21 ,35 ,40 ,37],[20, 38, 43, 29],[21, 30 ,39 ,38],[20 ,31, 50, 44] ] )
+print(twoDArray)
+
+# time complexity will be m*n
