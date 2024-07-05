8536d200611741e17acf0fae50252a052c083dd0
affanm16 <affan24748@gmail.com>
2024-07-06 02:50:23 +0530

    creating tuple

---
+++ 04_tuples/107_create_tuple.py
@@ -0,0 +1,11 @@
+new_tuple=('a','b','c')
+print(new_tuple)
+
+#for single valued we need to put a comma if not python treats it as string
+t1=('a',)
+print(type(t1))
+
+#method 2 using builtin function
+tuple1=tuple('abcde')
+print(tuple1)
+
