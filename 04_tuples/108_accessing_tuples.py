22da7b2dbeb7de7fbecc2fdd54d4e1c2b053b13b
affanm16 <affan24748@gmail.com>
2024-07-06 02:57:42 +0530

    accessing tuples

---
+++ 04_tuples/108_accessing_tuples.py
@@ -0,0 +1,16 @@
+#using bracket operator
+tuple1=('a','b','c')
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
+print(tuple1[-1])
+print(tuple1[-2])
+
+#slice operator
+print(tuple1[1:3])
+print(tuple1[2:0:-1])
+print(tuple1[::-1])
+
+
+
+# tuple1[0]='f' it cannot be changed
