7a9985e1f6caa81e9fabc51a1ed756ee4d05496d
affanm16 <affan24748@gmail.com>
2024-07-08 21:34:20 +0530

    printing format

---
+++ 07_Circular_single_linked_list/167_printing_csll.py
@@ -0,0 +1,54 @@
+#append method adds a single element at theend
+
+
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def append(self,value):
+        new_node=Node(value)
+        if self.length==0:
+            self.head=new_node
+            self.tail=new_node
+            new_node.next=new_node
+        else:
+            self.tail.next=new_node
+            new_node.next=self.head
+            self.tail=new_node
+        self.length+=1
+
+
+    def __str__(self):
+        temp_node=self.head
+        result=''
+        while temp_node is not None:
+            result+=str(temp_node.value)
+            temp_node=temp_node.next
+            if temp_node==self.head:
+                break
+            result+='->'
+        return result
+
+
+
+
+
+cslinkedlist=CircularSinglyLinkedList()
+
+
+cslinkedlist.append(10)
+cslinkedlist.append(20)
+cslinkedlist.append(30)
+print(cslinkedlist)
+# print(cslinkedlist.head.value)
+# print(cslinkedlist.head.next.value)
+
+#time complexity of append method O(1) 
+#space complexity of append method  O(1)
