a7f8684d449db3de84f460e889ce67c12cfabd44
affanm16 <affan24748@gmail.com>
2024-07-08 18:34:11 +0530

    inserting at the beginning

---
+++ 06_Linked_lists/138_insertion_at_begining.py
@@ -0,0 +1,61 @@
+# insertion in linked list
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def append(self,value):
+        new_node=Node(value)
+        #for adding in empty linked list
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length += 1
+    
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+
+    def __str__(self):
+         temp_node=self.head
+         result=''
+         while temp_node is not None:   #none marks end of linked list
+             result+=str(temp_node.value)
+             if temp_node.next is not None:
+                 result+=' -> '
+             temp_node=temp_node.next
+         return result
+             
+                 
+
+
+
+
+new_linked_list=LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+print(new_linked_list)
+new_linked_list.prepend(-10)
+print(new_linked_list)
+# print(new_linked_list.head.value)
+# print(new_linked_list.tail.value)
+
+#time and space complexity is O(1)
+
+
